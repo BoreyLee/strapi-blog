b108c9e0d8837b2f3a402c533a9d31374c509787
BoreyLee <475773037@qq.com>
2022-01-07 18:19:14 +0800

    Update database.js
    修改数据库配置

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "mysql",
-        host: env("DATABASE_HOST", "%"),
+        host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 3306), // 服务器端口
         database: env("DATABASE_NAME", "blog"), // 远程服务名称
         username: env("DATABASE_USERNAME", "borey"), // 账号
-        password: env("DATABASE_PASSWORD", "borey0529"), // 密码*AD258E9CA2F37846005DB1C01B263022EAD63E73
+        password: env("DATABASE_PASSWORD", "*AD258E9CA2F37846005DB1C01B263022EAD63E73"), // 密码
       },
       options: {},
     },
