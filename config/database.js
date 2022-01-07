7150a8b0a418c8bde7d3754c700661774f7cfca4
BoreyLee <475773037@qq.com>
2022-01-07 15:39:04 +0800

    feat: first commit

---
+++ config/database.js
@@ -0,0 +1,33 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306), // 服务器端口
+        database: env("DATABASE_NAME", "blog"), // 远程服务名称
+        username: env("DATABASE_USERNAME", "borey"), // 账号
+        password: env("DATABASE_PASSWORD", "borey0529"), // 密码*AD258E9CA2F37846005DB1C01B263022EAD63E73
+      },
+      options: {},
+    },
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "sqlite",
+//         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dde13445-b340-4328-abc6-bcc4b6f9a20e'
+};
